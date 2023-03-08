9d39c3d3ce87cd90797a8e893a6631e9fffc0df0
wangpeng20 <wangpeng20@baidu.com>
2023-03-08 17:31:53 +0800

    fix

---
+++ test_proxy.py
@@ -0,0 +1,27 @@
+import _env
+import os
+os.environ["OPENAI_API_BASE"]=_env.openai_api_base
+import openai
+openai.api_key="asjkajs"
+import configparser
+
+
+question="write a python code for quick sort"
+
+
+openai.log="debug"
+
+context={
+        "messages":[
+            ]
+        }
+context["messages"].append(
+        {"role": "user", "content": question}
+        )
+print(context)
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=context["messages"],
+    temperature=0,
+)
+print(response)
