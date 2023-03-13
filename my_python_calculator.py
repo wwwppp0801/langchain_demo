af8f91a99052464a5b9334d644a8fe31f42319da
wangpeng20 <wangpeng20@baidu.com>
2023-03-13 15:24:31 +0800

    fix

--- my_python_calculator.py
+++ my_python_calculator.py
@@ -1,3 +1,5 @@
+import _env
+import subprocess
 from langchain.agents.tools import Tool
 from langchain.chains.llm_math.base import LLMMathChain
 from langchain.llms.base import BaseLLM
@@ -61,7 +63,7 @@ def _get_my_llm_math(llm: BaseLLM) -> BaseTool:
 
 def get_python_coder_tool() -> BaseTool:
     name="PythonCoder"
-    description="you can write python code to solve the problem,you must write, you MUST write a complete python program , print the answer to stdout"
+    description="you can write python code to solve the problem,you must write, you must write a complete python program , print the answer to stdout"
     def extract_code_blocks(string):
         import re
         pattern = r'```(?:python)?\s*(.*?)\s*```' 
@@ -70,16 +72,18 @@ def get_python_coder_tool() -> BaseTool:
         return code_blocks
 
     def run_python_code(code:str):
-        python_executor = PythonREPL()
+        #python_executor = PythonREPL()
         #code = code.split("```")[1].strip("\n\t\r ")
+        #print(code)
         code_blocks=extract_code_blocks(code)
         
         if len(code_blocks)==0:
             return "no code for "+name+" to resolve the problem"
             #raise NotImplementedError("no code here",code)
-        output = python_executor.run("\n".join(code_blocks))
-        #print(code)
-        #print(output)
+        code_str="\n".join(code_blocks)
+        p = subprocess.Popen([_env.python_path, '-c', code_str], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        output = out.decode() + err.decode()
         return output
 
 
@@ -94,26 +98,37 @@ def get_python_coder_tool() -> BaseTool:
     )
 if __name__=="__main__":
     code='''
-
 ```
-import hashlib
 
-string = "123456"
-hash_object = hashlib.md5(string.encode())
-print(hash_object.hexdigest())
+import sympy as sp
+
+x = sp.Symbol('x')
+f = 5*x**2 + 8*x - 16
+f_prime = sp.diff(f, x)
+critical_points = sp.solve(f_prime, x)
+second_derivatives = [sp.diff(f, x, 2).subs(x, cp) for cp in critical_points]
+
+for i in range(len(critical_points)):
+    if second_derivatives[i] > 0:
+        print(f"The critical point {critical_points[i]} is a minimum")
+    elif second_derivatives[i] < 0:
+        print(f"The critical point {critical_points[i]} is a maximum")
+    else:
+        print(f"The critical point {critical_points[i]} is an inflection point")
 ```
+
 '''
     result=get_python_coder_tool()._run(code)
     
-    code='''
-
-```python
-import hashlib
-
-string = "123456"
-hash_object = hashlib.md5(string.encode())
-print(hash_object.hexdigest())
-```
-'''
-    result=get_python_coder_tool()._run(code)
+#    code='''
+#
+#```python
+#import hashlib
+#
+#string = "123456"
+#hash_object = hashlib.md5(string.encode())
+#print(hash_object.hexdigest())
+#```
+#'''
+#    result=get_python_coder_tool()._run(code)
     print(result)
