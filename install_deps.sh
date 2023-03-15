34441ac5d879810d749039d60b7fae8dac9ca749
wangpeng20 <wangpeng20@baidu.com>
2023-03-15 19:39:06 +0800

    fix

--- install_deps.sh
+++ install_deps.sh
@@ -1,32 +1,36 @@
 
+suffix=" -i https://pypi.tuna.tsinghua.edu.cn/simple"
 
 # 依赖
 
-pip3 install langchain==0.0.100  -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip3 install openai  -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip3 install google-search-results  -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip3 install wolframalpha  -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip3 install nltk  -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip3 install langchain==0.0.100  $suffix
+pip3 install openai  $suffix
+pip3 install google-search-results  $suffix
+pip3 install wolframalpha  $suffix
+pip3 install nltk  $suffix
 
 # PythonCoder依赖
-pip3 install sympy  -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip3 install wikipedia -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip3 install bs4 -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip3 install sympy  $suffix
+pip3 install wikipedia $suffix
+pip3 install bs4 $suffix
 
 
 # webui依赖
 
 pip3 install flask
-pip3 install flask_socketio  -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip3 install flask_socketio  $suffix
+
+#performance.sh依赖
+pip3 install pandas $suffix
 
 
 # 文件搜索依赖
-pip3 install "unstructured[local-inference]" -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip3 install "unstructured[local-inference]" $suffix
 
-pip3 install 'git+https://github.com/facebookresearch/detectron2.git' -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip3 install 'git+https://github.com/facebookresearch/detectron2.git'
 
-pip3 install libmagic -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip3 install chromadb -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip3 install libmagic $suffix
+pip3 install chromadb $suffix
 
 os=$(uname -s)
 if [ "$os" = "Linux" ]; then
