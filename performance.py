6eb8b29c96c37114b5bd990f6d30c2c81d19a38c
wangpeng20 <wangpeng20@baidu.com>
2023-03-16 20:49:33 +0800

    add performance

--- performance.py
+++ performance.py
@@ -8,6 +8,7 @@ import os
 import threading
 import queue
 import re
+import pandas as pd
 
 def ansi_escape(line:str):
     ansi_esc = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
@@ -62,7 +63,7 @@ def test_a_query(command:str,tools="search,python_coder",verbose=True,debug=Fals
     t3.start()
 
     # 定义一个超时时间（秒）
-    timeout = 120
+    timeout = 180
 
     # 记录开始时间
     start_time = time.time()
@@ -112,6 +113,8 @@ def write_file(s:str,filename:str):
         file = open(filename, "w")
         file.write(s)
         file.close()
+        df_json = pd.read_json(filename)
+        df_json.to_excel(filename+'.xlsx')
 
 def get_final_answer(my_string,sub_string="Final Answer:"):
     last_index = my_string.rfind(sub_string) # find the last index of sub_string
@@ -126,62 +129,74 @@ def get_llm_count(raw_result):
     count = raw_result.count(sub_string) # find the last index of sub_string
     return count+1
 
+
+def validate(i:str,validator:str)->bool:
+    #print(validator)
+    def m(regexp:str)->bool:
+        return re.search(regexp, i, re.IGNORECASE) != None
+    try:
+        return eval(validator)
+    except:
+        return False
+
 if __name__=="__main__":
     print ("start")
-    querys=[
-            "google的总市值是多少",
-            "Who is the current leader of Japan? What is the largest prime number that is smaller than their age",
-            "什么是比特币？它是如何创造出来的？",
-            "谁发现了苯和氨基酸的分子式？",
-            "2023年，谁最有可能是中国的总理",
-            "去年谁是中国的总理",
-            "2000年,谁是中国的总理",
-            "谁是中国历史上任期最长的总理",
-            "周恩来当中国国家总理总共多长时间？",
-            "2023年，速度最快的显卡是什么？价格是多少？",
-            "2023年，价格最贵的显卡是什么？价格是多少？",
-            "中国人里，最有名的打过NBA的球员, 现在在干啥？",
-            "把圆周率计算到小数点后1000位",
-            "圆周率，保留小数点后前1000位",
-            "地球与太阳的距离，是土星与太阳的距离的几倍？",
-            "地球与太阳的距离，与土星与太阳的距离的比例？",
-            "地球与太阳的距离，是水星与太阳的距离的几倍？",
-            "地球与太阳的距离，与水星与太阳的距离的比例？",
-            "把太阳系的行星按照质量排序",
-            "人类发现的最大的恒星，按照质量排序的前十名是哪些？",
-            "人类发现的最大的恒星，按照质量排序的前十名是哪些？",
-            "著名的小提琴协奏曲《贝多芬》是由哪位作曲家创作的？它的调号是什么？",
-            "Which composer wrote the famous violin concerto \"Beethoven\"? What is its key signature?",
-            " What is the largest prime number that is smaller than 1293812746",
-            "北京今天的温度是多少摄氏度？",
-            "推荐5首周杰伦在2002年之前创作的歌",
-            "推荐1首周杰伦在2002年之前创作的歌",
-            "把1234分解质因数",
-            "Factor 1234 into prime factors",
-            "求 x^2+9x-5 的最小值点",
-            "已知引力常量为G，地球质量为M，地球半径为R，地球表面重力加速度为g，空间站质量为m，空间站在轨道上做圆周运动的向心加速度是多少？",
-            "已知引力常量为G，地球质量为M，地球半径为R，地球表面重力加速度为g，空间站质量为m，空间站在轨道上做圆周运动的周期是多少？" ,
-            "截面为等腰直角三角形的圆锥侧面展开图的圆心角弧度为",
-            "如果（sin a + cos a）/（sin a - cos a）=-3，则tan 2a=",
-            "Roger has 5 tennis balls. He buys 2 more cans of tennis balls. Each can has 3 tennis balls. How many tennis balls does he have now?",
-            "The cafeteria had 23 apples. If they used 20 to make lunch and bought 6 more, how many apples do they have?",
-            "It takes Amy 4 minutes to climb to the top of a slide. It takes her 1 minute to slide down. The water slide closes in 15 minutes. How many times can she slide before it closes?",
-            "A needle 35 mm long rests on a water surface at 20◦C. What force over and above the needle’s weight is required to lift the needle from contact with the water surface? σ = 0.",
-            "一个钱包里有100个五分和十分硬币。硬币的总价值是7美元。钱包里有多少个每种硬币？",
-            "There are 100 coins in denominations of 5 cents and 10 cents. The total value of the coin is 7 dollas. How many coins are there each?",
-            "who setup Y Combinator",
-            "Y Combinator的现任ceo是谁",
-            "北京天气",
-            "北京的完整天气，用摄氏度返回",
-            "git pull有冲突应该怎么处理？",
-            ]
+#    querys=[
+#            "google的总市值是多少",
+#            "Who is the current leader of Japan? What is the largest prime number that is smaller than their age",
+#            "什么是比特币？它是如何创造出来的？",
+#            "2023年，谁最有可能是中国的总理",
+#            "去年谁是中国的总理",
+#            "2000年,谁是中国的总理",
+#            "谁是中国历史上任期最长的总理",
+#            "周恩来当中国国家总理总共多长时间？",
+#            "2023年，速度最快的显卡是什么？价格是多少？",
+#            "2023年，价格最贵的显卡是什么？价格是多少？",
+#            "中国人里，最有名的打过NBA的球员, 现在在干啥？",
+#            "把圆周率计算到小数点后1000位",
+#            "圆周率，保留小数点后前1000位",
+#            "地球与太阳的距离，是土星与太阳的距离的几倍？",
+#            "地球与太阳的距离，与土星与太阳的距离的比例？",
+#            "地球与太阳的距离，是水星与太阳的距离的几倍？",
+#            "地球与太阳的距离，与水星与太阳的距离的比例？",
+#            "把太阳系的行星按照质量排序",
+#            "人类发现的最大的恒星，按照质量排序的前十名是哪些？",
+#            "人类发现的最大的恒星，按照质量排序的前十名是哪些？",
+#            "著名的小提琴协奏曲《贝多芬》是由哪位作曲家创作的？它的调号是什么？",
+#            "Which composer wrote the famous violin concerto \"Beethoven\"? What is its key signature?",
+#            " What is the largest prime number that is smaller than 1293812746",
+#            "北京今天的温度是多少摄氏度？",
+#            "推荐5首周杰伦在2002年之前创作的歌",
+#            "推荐1首周杰伦在2002年之前创作的歌",
+#            "把1234分解质因数",
+#            "Factor 1234 into prime factors",
+#            "求 x^2+9x-5 的最小值点",
+#            "已知引力常量为G，地球质量为M，地球半径为R，地球表面重力加速度为g，空间站质量为m，空间站在轨道上做圆周运动的向心加速度是多少？",
+#            "已知引力常量为G，地球质量为M，地球半径为R，地球表面重力加速度为g，空间站质量为m，空间站在轨道上做圆周运动的周期是多少？" ,
+#            "截面为等腰直角三角形的圆锥侧面展开图的圆心角弧度为",
+#            "如果（sin a + cos a）/（sin a - cos a）=-3，则tan 2a=",
+#            "Roger has 5 tennis balls. He buys 2 more cans of tennis balls. Each can has 3 tennis balls. How many tennis balls does he have now?",
+#            "The cafeteria had 23 apples. If they used 20 to make lunch and bought 6 more, how many apples do they have?",
+#            "It takes Amy 4 minutes to climb to the top of a slide. It takes her 1 minute to slide down. The water slide closes in 15 minutes. How many times can she slide before it closes?",
+#            "A needle 35 mm long rests on a water surface at 20◦C. What force over and above the needle’s weight is required to lift the needle from contact with the water surface? σ = 0.",
+#            "一个钱包里有100个五分和十分硬币。硬币的总价值是7美元。钱包里有多少个每种硬币？",
+#            "There are 100 coins in denominations of 5 cents and 10 cents. The total value of the coin is 7 dollas. How many coins are there each?",
+#            "who setup Y Combinator",
+#            "Y Combinator的现任ceo是谁",
+#            "北京天气",
+#            "北京的完整天气，用摄氏度返回",
+#            "git pull有冲突应该怎么处理？",
+#            ]
+    with open('test_case.json', 'r') as f:
+        testcases = json.load(f)
     rows=[]
-    tools="search,python_coder"
+    tools="search,calculator,ch_en_translator"
     import sys
     if len(sys.argv)>=2:
         tools=sys.argv[1]
     filename="report"+datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")+"_"+tools+".json"
-    for query in querys:
+    for case in testcases:
+        query=case["query"]
         print(query)
         row={}
         result,errorlog=test_a_query(query,tools=tools,debug=True)
@@ -189,6 +204,9 @@ if __name__=="__main__":
         row['final_result']=get_final_answer(result)
         row['llm_count']=get_llm_count(result)
         row['raw_result']=result
+        if "validator" in case:
+            row['validator']=case["validator"]
+            row['validate_result']=validate(result,case["validator"])
         #print(result,errorlog)
         rows.append(row)
         raw_file_str=json.dumps(rows)
