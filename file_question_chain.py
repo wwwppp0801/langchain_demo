5161edb80019f619e0a4c9becd62a31ef561b38b
wangpeng20 <wangpeng20@baidu.com>
2023-03-20 11:29:41 +0800

    add openai embeddings

--- file_question_chain.py
+++ file_question_chain.py
@@ -6,6 +6,7 @@ from tools import file_search_tool
 import sys
 
 from termcolor import colored
+from langchain.embeddings import OpenAIEmbeddings
 
 if __name__ == '__main__': 
     if len(sys.argv)!=3:
@@ -16,12 +17,14 @@ if __name__ == '__main__':
     filename=sys.argv[2]
     print(sys.argv)
     llm = OpenAI(model_name=_env.model_name, temperature=0.0,openai_api_key=_env.api_key)
+    embeddings = OpenAIEmbeddings(openai_api_key=_env.api_key)
     
     tool = file_search_tool.CustomFileSearchTool(
             name = "File",
             #description = "you must use it first, when you need to answer questions about product after sale",
             description = "Any question must first use this tool, using the original question as Action Input",
             llm=llm,
+            embeddings=embeddings,
             )
     tool.load_from_file(filename)
 
