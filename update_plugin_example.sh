43c61a9a470c11dfe59ac616e6b04d2aae6aff75
wangpeng20 <wangpeng20@baidu.com>
2023-04-10 15:52:52 +0800

    update prompt ,update plugin example

--- update_plugin_example.sh
+++ update_plugin_example.sh
@@ -1,4 +1,5 @@
 cd plugin_profiles/built-in/iot2.dueros.com/
+rm test.zip
 zip -r test.zip  prompt  api.yaml  manifest.json  device_list.json
 cd -
-cp test.zip upload/plugin_example.zip
+cp plugin_profiles/built-in/iot2.dueros.com/test.zip upload/plugin_example.zip
