a75d22dca762e46934fb26945f45c308b3e018c1
wangpeng20 <wangpeng20@baidu.com>
2023-04-11 12:15:26 +0800

    fix

--- update_plugin_example.sh
+++ update_plugin_example.sh
@@ -1,5 +1,5 @@
 cd plugin_profiles/built-in/iot2.dueros.com/
 rm test.zip
-zip -r test.zip  prompt  api.yaml  manifest.json  device_list.json
+zip -r test.zip  prompt  api.yaml  manifest.json  device_list.json devicelist_detail.txt
 cd -
 cp plugin_profiles/built-in/iot2.dueros.com/test.zip upload/plugin_example.zip
