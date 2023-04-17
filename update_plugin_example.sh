c1f6ca85abf7778a3b6df5a801af5d688de5e6a9
wangpeng20 <wangpeng20@baidu.com>
2023-04-17 18:48:17 +0800

    run_test_cases_iot example

--- update_plugin_example.sh
+++ update_plugin_example.sh
@@ -3,3 +3,11 @@ rm test.zip
 zip -r test.zip  prompt  api.yaml  manifest.json  device_list.json devicelist_detail.txt
 cd -
 cp plugin_profiles/built-in/iot2.dueros.com/test.zip upload/plugin_example.zip
+
+
+
+cd plugin_profiles/built-in/iot3.dueros.com/
+rm test.zip
+zip -r test.zip  prompt  api.yaml  manifest.json  device_list.json devicelist_detail.txt
+cd -
+cp plugin_profiles/built-in/iot3.dueros.com/test.zip upload/plugin_example_iot3.zip
